f951a00c39a6b54fd972c8819cb35a325c0f21c7
acoubete <acoubete@cst-tp-b10309>
2019-11-21 09:10:32 +0100

    Initialiser une classe

--- src/main/java/NewClass1.java
+++ src/main/java/NewClass1.java
@@ -9,5 +9,23 @@
  * @author Marie
  */
 public class NewClass1 {
-    
+    private String nom;
+    private int age;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+   
 }
